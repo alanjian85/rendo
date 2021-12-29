7a6c04c50346be0adf774d059172a32875133186
Alan Jian <alanjian85@outlook.com>
2021-12-29 19:00:19 +0800

    Update vector3

--- include/vector3.hpp
+++ include/vector3.hpp
@@ -1,59 +1,85 @@
 #ifndef SENTINEL_DETAIL_VECTOR3_HPP
 #define SENTINEL_DETAIL_VECTOR3_HPP
 
+#include <cstdlib>
 #include <cmath>
 
-#include "vector.hpp"
-
 namespace sentinel {
-    template <typename T>
-    struct vector<T, 3> {
-        friend vector operator+(vector lhs, vector rhs) noexcept {
-            return {lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z}; 
+    struct vector3 { 
+        constexpr vector3() 
+            : x(), y(), z() 
+        {
+        
         }
-
-        friend vector operator-(vector lhs, vector rhs) noexcept {
-            return {lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z};
-        }
-
-        friend vector operator+(vector lhs, T rhs) noexcept {
-            return {lhs.x + rhs, lhs.y + rhs, lhs.z + rhs};
-        }
-
-        friend vector operator-(vector lhs, T rhs) noexcept {
-            return {lhs.x - rhs, lhs.y - rhs, lhs.z - rhs};
-        }
-
-        friend vector operator*(vector lhs, T rhs) noexcept {
-            return {lhs.x * rhs, lhs.y * rhs, lhs.z * rhs}; 
-        } 
-
-        friend vector operator/(vector lhs, T rhs) noexcept {
-            return {lhs.x / rhs, lhs.y / rhs, lhs.z / rhs};
-        }
-
-        constexpr vector() = default;
-        constexpr vector(T x_, T y_, T z_) 
-            : x(x_),
-              y(y_),
-              z(z_)
+        constexpr vector3(double x_, double y_, double z_) 
+            : x(x_), y(y_), z(z_)
         {
             
         }
 
-        constexpr vector operator-() const noexcept {
+        constexpr double& operator[](std::size_t index) noexcept {
+            return index == 0 ? x : index == 1 ? y : z;
+        }
+
+        constexpr const double& operator[](std::size_t index) const noexcept {
+            return index == 0 ? x : index == 1 ? y : z; 
+        }
+
+        constexpr vector3 operator-() const noexcept {
             return {-x, -y, -z};
         }
 
-        constexpr T length_squared() const noexcept {
+        constexpr vector3& operator+=(vector3 rhs) noexcept {
+            x += rhs.x;
+            y += rhs.y;
+            z += rhs.z;
+            return *this;
+        }
+
+        constexpr vector3& operator-=(vector3 rhs) noexcept {
+            x -= rhs.x;
+            y -= rhs.y;
+            z -= rhs.z;
+            return *this;
+        }
+
+        constexpr vector3& operator+=(double rhs) noexcept {
+            x += rhs;
+            y += rhs;
+            z += rhs;
+            return *this;
+        }
+
+        constexpr vector3& operator-=(double rhs) noexcept {
+            x -= rhs;
+            y -= rhs;
+            z -= rhs;
+            return *this;
+        }
+
+        constexpr vector3& operator*=(double rhs) noexcept {
+            x *= rhs;
+            y *= rhs;
+            z *= rhs;
+            return *this;
+        }
+
+        constexpr vector3& operator/=(double rhs) noexcept {
+            x /= rhs;
+            y /= rhs;
+            z /= rhs;
+            return *this;
+        }
+
+        constexpr double length_squared() const noexcept {
             return x * x + y * y + z * z;
         }
 
-        constexpr T length() const noexcept {
+        constexpr double length() const noexcept {
             return std::sqrt(length_squared());
         }
 
-        constexpr vector normalize() const noexcept {
+        constexpr vector3 normalize() const noexcept {
             auto len = length();
             if (len == 0)
                 return {0, 0, 0};
@@ -61,10 +87,34 @@ namespace sentinel {
                 return {x / len, y / len, z / len};
         }
 
-        T x;
-        T y;
-        T z;
+        double x;
+        double y;
+        double z;
     };
+
+    constexpr inline vector3 operator+(vector3 lhs, vector3 rhs) noexcept {
+        return {lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z};
+    }
+
+    constexpr inline vector3 operator-(vector3 lhs, vector3 rhs) noexcept {
+        return {lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z};
+    }
+
+    constexpr inline vector3 operator+(vector3 lhs, double rhs) noexcept {
+        return {lhs.x + rhs, lhs.y + rhs, lhs.z + rhs};
+    }
+
+    constexpr inline vector3 operator-(vector3 lhs, double rhs) noexcept {
+        return {lhs.x - rhs, lhs.y - rhs, lhs.z - rhs};
+    }
+
+    constexpr inline vector3 operator*(vector3 lhs, double rhs) noexcept {
+        return {lhs.x * rhs, lhs.y * rhs, lhs.z * rhs};
+    }
+
+    constexpr inline vector3 operator/(vector3 lhs, double rhs) noexcept {
+        return {lhs.x / rhs, lhs.y / rhs, lhs.z / rhs};
+    }
 }
 
 #endif
