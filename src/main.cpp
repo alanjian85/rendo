31851ec205f0a9ec19f7bc43baa5e1fc6fa5c096
Alan Jian <alanjian85@outlook.com>
2022-01-17 20:49:17 +0800

    Load all skybox textures

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,18 @@ public:
           lookat_({0, 0, 3}, {0, 0, 0}, {0, 1, 0}),
           rotate_(rad(45), {1, 1, 1})
     {
-        texture_.load("res/textures/texture.ppm");
-        sampler_.bind_texture(texture_);
+        skybox_right_.load("res/textures/right.ppm");
+        skybox_left_.load("res/textures/left.ppm");
+        skybox_top_.load("res/textures/top.ppm");
+        skybox_bottom_.load("res/textures/bottom.ppm");
+        skybox_back_.load("res/textures/back.ppm");
+        skybox_front_.load("res/textures/front.ppm");
+        sampler_.bind_right(skybox_right_);
+        sampler_.bind_left(skybox_left_);
+        sampler_.bind_top(skybox_top_);
+        sampler_.bind_bottom(skybox_bottom_);
+        sampler_.bind_back(skybox_back_);
+        sampler_.bind_front(skybox_front_);
     }
     
     virtual vertex vert(const vertex& vert) override {
@@ -22,11 +32,17 @@ public:
     }
 
     virtual color_rgba frag(const vertex_data& data) override {
-        return sampler_(data.uv);
+        return {1, 0, 0, 1};
     }
 private:
-    texture texture_;
-    sampler2 sampler_;
+    texture skybox_right_;
+    texture skybox_left_;
+    texture skybox_top_;
+    texture skybox_bottom_;
+    texture skybox_back_;
+    texture skybox_front_;
+    sampler_cube sampler_;
+
     persp persp_;
     lookat lookat_;
     rotate rotate_;
