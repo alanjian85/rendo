da086c077356a1169078c1e4adb4158303933727
Alan Jian <alanjian85@outlook.com>
2022-01-22 21:25:27 +0800

    Aqua diffuse

--- src/main.cpp
+++ src/main.cpp
@@ -12,27 +12,26 @@ public:
     hello_shader(double aspect, const model& m) 
         : model_(m),
           persp_(rad(45), aspect, 0.1, 100),
-          lookat_({0, 0.075, 0.5}, {0, 0.075, 0}, {0, 1, 0}),
-          rotate_(rad(45), {0, 1, 0})
+          lookat_({0, 1.5, 9}, {0, 1.5, 0}, {0, 1, 0})
     {
 
     }
 
     virtual vector4 vert(int n) override {
         v_normals[n % 3] = model_.get_normal(n);
-        return persp_ * lookat_ * rotate_ * model_.get_vertex(n);
+        return persp_ * lookat_ * model_.get_vertex(n);
     }
 
     virtual color_rgba frag(vector3 bar) override {
         auto normal = frag_lerp(v_normals, bar);
-        return {normal.x / 2 + 0.5, normal.y / 2 + 0.5, normal.z / 2 + 0.5, 1};
+        auto t = std::max(dot(normal, {1, 1, 1}), 0.0);
+        return {t, t, t, 1};
     }
 private:
     const model& model_;
 
     persp persp_;
     lookat lookat_;
-    rotate rotate_;
 
     vector3 v_normals[3];
 };
@@ -42,7 +41,7 @@ int main() {
     r.clear({0.627, 0.906, 0.898, 1.0});
 
     model m;
-    m.load("res/models/bunny.obj");
+    m.load("res/models/aqua.obj");
 
     hello_shader s(r.aspect(), m);
     r.render(m.num_vertices(), s);
