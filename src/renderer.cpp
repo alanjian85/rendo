a292dbdbd9a9e8402f76d6431f2180fb4e80cc09
Alan Jian <alanjian85@outlook.com>
2022-01-23 16:03:40 +0800

    Untrack output dir

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -42,8 +42,8 @@ void renderer::render_triangle(triangle t, basic_shader& s) {
             auto z = bc_clip.x * t[0].z + bc_clip.y * t[1].z + bc_clip.z * t[2].z;
 
             if (fb_.depth_test(x, y, z)) {
-                auto color = s.frag(bc_screen);
-                if (color.a == 0)
+                auto color = s.frag(bc_clip);
+                if (color.a < 0.1)
                     continue;
                 fb_(x, y).color = color;
                 fb_(x, y).depth = z;
