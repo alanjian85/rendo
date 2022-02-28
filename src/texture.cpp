4519911d15267f5031e2209c9d62154e61eac351
Alan Jian <alanjian85@outlook.com>
2022-02-28 13:21:08 +0800

    Update wrapping algorithm

--- src/texture.cpp
+++ src/texture.cpp
@@ -100,22 +100,18 @@ color_rgba sampler2::operator()(vector2 uv) const {
 
     switch (wrap()) {
     case wrapping::repeat:
-        if (uv.x < 0 || uv.x > 1)
-            uv.x = uv.x - std::floor(uv.x);
-        if (uv.y < 0 || uv.y > 1)
-            uv.y = uv.y - std::floor(uv.y);
+        double temp;
+        uv.x = std::modf(uv.x, &temp);
+        uv.y = std::modf(uv.y, &temp);
         break;
     case wrapping::repeat_mirrored:
-        if (static_cast<int>(uv.x) % 2 == 0) {
-            uv.x = uv.x - std::floor(uv.x);
-        } else {
-            uv.x = std::ceil(uv.x) - uv.x;
-        }
-        if (static_cast<int>(uv.y) % 2 == 0) {
-            uv.y = uv.y - std::floor(uv.y);
-        } else {
-            uv.y = std::ceil(uv.y) - uv.y;
-        }
+        double intx, inty;
+        uv.x = std::modf(uv.x, &intx);
+        uv.y = std::modf(uv.y, &inty);
+        if (static_cast<int>(intx) % 2 == 1)
+            uv.x = 1 - uv.x;
+        if (static_cast<int>(inty) % 2 == 1)
+            uv.y = 1 - uv.y;
     case wrapping::clamp_to_edge:
         uv.x = std::clamp(uv.x, 0.0, 1.0);
         uv.y = std::clamp(uv.y, 0.0, 1.0);
